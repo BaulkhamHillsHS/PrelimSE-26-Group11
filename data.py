149379f0d1b9831381d73a7e80766e13b992d8d9
Duocuokie <duocuokie@gmail.com>
2026-06-01 23:06:49 +1000

    planned out basic data structures

---
+++ data.py
@@ -0,0 +1,28 @@
+"""
+PLAN_1 = 0
+
+credentials = {
+   0 : {
+        username : "",
+        password : "",
+        plan : PLAN_1,
+        credit_card : 1234567891000000
+        profiles : {
+            0 : {
+                name : "",
+                age : "",
+                watchlist : ""
+            }
+        }
+   }
+}
+
+
+medias = {
+    0 : {
+        title : "",
+        type : SHOW
+        video : [image, image]
+    }
+}
+"""
