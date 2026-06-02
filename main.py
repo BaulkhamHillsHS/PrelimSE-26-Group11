52a4275f695c193557578f0023d3eee2c26b4a71
cracker-schnitzel <guillermo.guedez@education.nsw.gov.au>
2026-06-02 12:29:38 +1000

    Update main.py
    add window (empty and wip title)

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import customtkinter as ctk
+import tkinter as tk
 
+# just for clarity
+ctk.set_appearance_mode("system")
+ctk.set_default_color_theme("dark-blue")
+
+class StreamingApp(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("WIP Streaming App Jeremy Guillermo")
+        self.geometry("720x540")
+        self._build_ui()
+
+    def _build_ui(self):
+        pass
+    
+if __name__ == "__main__":
+    app = StreamingApp()
+    app.mainloop()
