7f2be8f570eadd3f9f36522abea75eee6a2dd766
Duocuokie <duocuokie@gmail.com>
2026-06-02 22:26:58 +1000

    added basic manager objects

--- data.py
+++ data.py
@@ -8,30 +8,40 @@ HOME = 2
 VIEW = 3
 SUBSCRIBE = 5
 # Genres
-
+GEX = 0
+THE = 1
+GECKO = 2
+# types
+SHOW = 0
+MOVIE = 1
+# ratings
+G = 0
+PG = 1
+M = 2
+MA = 3
+R = 4
+X = 5
 
 
 credentials = {
-   0 : {
-        username : "",
-        password : "",
-        plan : PLAN_1,
-        credit_card : 1234567891000000,
-        profiles : {
-            0 : {
-                name : "",
-                age : "",
-                watchlist : ""
-            }
-        }
-   }
+    -100: {
+        "username": "Gex",
+        "password": "123457890",
+        "plan": PLAN_1,
+        "credit_card": 1234567891000000,
+        "profiles": {0: {"name": "", "age": "", "watchlist": ""}},
+    }
 }
 
 
 media = {
-    0 : {
-        title : "",
-        type : SHOW
-        video : [image, image]
+    "example": {
+        "title": "",
+        "type": SHOW,
+        "display_path": "path",
+        "thumbnail": "path",
+        "length_sec": 1000,
+        "rating": M,
+        "genre": [GEX, THE, GECKO],
     }
 }
--- main.py
+++ main.py
@@ -6,48 +6,62 @@ import data
 
 
 class SceneManager():
-    def __init__(self, ctk_app : ctk.ctk):
+    def __init__(self, ctk_app : ctk.CTk):
         self.ctk_app = ctk_app
-    def switch_scene(self, scene, *args): -> None
+    def switch_scene(self, scene, *args) -> None:
         # match LOGIN:
         #   self.ctk_app.buildlogin()
+        a = 1
+        pass
+
+class AccountManager():
+    def __init__(self) -> None:
+        self.profile_count = 0
+        self.current_profile = None
+    def register_user(self):
+        current_profile = 0
+        data.credentials[0] = {
+            "username": "Gex",
+            "password": "123457890",
+            "plan": data.PLAN_1,
+            "credit_card": 1234567891000000,
+            "profiles": {0: {"name": "", "age": "", "watchlist": ""}},
+        }
+        self.profile_count += 1
+    def login(self):
+        pass
+    def logout(self):
+        pass
+    def add_profile(self):
         pass
 
 class Media():
     def __init__(self, id) -> None:
-        self.m_data = data.media[id]
-        self.title = m_data[title]
-        self.display_path = m_data[path]
-    
-        self.thumbnail = m_data[thumbnail]
-        self.length_sec = m_data[length_sec]
-        self.rating = m_data[rating]
-        self.genre = m_data[genre]
+        m_data = data.media[id]
+        self.title = m_data["title"]
+        self.display_path = m_data["display_path"]
+        self.thumbnail = m_data["thumbnail"]
+        self.length_sec = m_data["length_sec"]
+        self.rating = m_data["rating"]
+        self.genre = m_data["genre"]
 
-    def get_display(self) -> ctk.CTkImage:
+    def build_card(self) -> None:
         pass
 
-    def build_card(self) -> void:
-        
-        
 
 class Movie(Media):
-    def __init__(self, id) -> None:
-        super.__init__(self, id)
-    
-    def get_display(self) -> ctk.CTkImage:
-        if display == None:
-            display =  customtkinter.CTkImage(light_image=Image.open(self.display_path[key]),
-                                 size=(30, 30))
-        return display
+    def __init__(self, id):
+        super.__init__(id)
+        self.display = ctk.CTkImage(light_image=Image.open(self.display_path), size=(30, 30))
+
 
 class Show(Media)
-    def __init__(self, id) -> None:
-        super.__init__(self, id)
-        self.display = None
-    
-    def get_display(self, key) -> ctk.CTkImage:
-        if display[key] == None:
-            display[key] =  customtkinter.CTkImage(light_image=Image.open(self.display_path[key]),
-                                 size=(30, 30))
-        return display[key]
+    def __init__(self, id):
+        super.__init__(id)
+        self.display = []
+        for path in self.display_path:
+            self.display.append(ctk.CTkImage(light_image=Image.open(path), size=(30, 30)))
+
+
+
+scene_manager = SceneManager()
